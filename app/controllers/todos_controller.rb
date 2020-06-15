dcfc186a068583e4c4de375db63b4eb0a1c99db8
Oleg <oleg@Olegs-MacBook-Pro.local>
2020-06-15 00:55:14 -0700

    dding edit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,9 +12,26 @@ class TodosController < ApplicationController
            render 'new'
         end
     end
+    
     def show
         @todo = Todo.find(params[:id])
     end
+
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        if @todo.update(todo_params)
+            flash[:notice] = "Succesfull update"
+            redirect_to todo_path(@todo)
+        else
+            flash[:notice] = "It was a problem with the update"
+            render 'edit'
+        end
+    end
+
     private 
     def todo_params
         params.require(:todo).permit(:name, :description)
